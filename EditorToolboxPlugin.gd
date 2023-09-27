300b5f0e7268dae6c47494fe59fa982391572ed0
Paul <arr.explore@gmail.com>
2023-09-27 04:26:00 -0400

    Adding a screenshot command (not yet working)

--- EditorToolboxPlugin.gd
+++ EditorToolboxPlugin.gd
@@ -1,24 +1,44 @@
 @tool
 extends EditorPlugin
 
-func _input(event):
-	if event is InputEventKey:
-		if event.is_pressed() and !event.is_echo():
-			if event.ctrl_pressed and event.alt_pressed and event.keycode == KEY_T:
-				print_test_message()
-				get_viewport().set_input_as_handled()
+var submenu_command: PopupMenu
+var submenu_creation: PopupMenu
 
 func _enter_tree():
-	add_tool_menu_item("Create BaseMaterial3D", Callable(self, "create_base_material_3d"))
-	add_tool_menu_item("Print Test (Ctrl+Alt+T)", Callable(self, "print_test_message"))
+	submenu_command = PopupMenu.new()
+	submenu_command.connect("id_pressed", Callable(self, "_on_command_submenu_item_selected"))
+	add_tool_submenu_item("Command", submenu_command)
+	submenu_command.add_item("Screenshot (Ctrl+K)", 0)
 	
-func _exit_tree():
-	remove_tool_menu_item("Create BaseMaterial3D")
-	remove_tool_menu_item("Print Test (Ctrl+Alt+T)")
+	submenu_creation = PopupMenu.new()
+	submenu_creation.connect("id_pressed", Callable(self, "_on_creation_submenu_item_selected"))
+	add_tool_submenu_item("Creation", submenu_creation)
+	submenu_creation.add_item("Create BaseMaterial3D", 0)
 
-func print_test_message():
-	print("Test message from plugin.")
+func _exit_tree():
+	remove_tool_menu_item("Command")
+	remove_tool_menu_item("Creation")
+
+func _input(event: InputEvent):
+	if event is InputEventKey:
+		if event.is_pressed() and !event.is_echo():
+			
+			if event.ctrl_pressed and event.keycode == KEY_K:
+				print("Calling screenshot()")
+				screenshot();
+
+func _on_command_submenu_item_selected(id: int):
+	if id == 0:
+		screenshot()
+
+func _on_creation_submenu_item_selected(id: int):
+	if id == 0:
+		create_base_material_3d()
 
 func create_base_material_3d():
-	var create_base_material = preload("res://addons/EditorToolbox/CreateBaseMaterial3D.gd").new()
-	create_base_material.execute()
+	var _instance = preload("res://addons/EditorToolbox/CreateBaseMaterial3D.gd").new()
+	_instance.execute()
+
+func screenshot():
+	var _instance = preload("res://addons/EditorToolbox/Screenshot.gd").new()
+	_instance.execute()
